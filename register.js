6fa464120301cb47fdcc93e5612643d91f9e2537
felipe hernandez <felipehernandez@felipes-MacBook-Pro.local>
2018-06-25 20:33:20 -0700

    update register page

---
+++ register.js
@@ -0,0 +1,10 @@
+  function registerBox(){
+  document.getElementById('stepOneBtn').addEventListener('click' , function(e){
+    let stepOne = document.getElementById("stepOne");
+    let stepTwo = document.getElementById("stepTwo");
+    stepOne.style.display = "none";
+    stepTwo.style.display = "block";
+  });
+
+  }
+  registerBox();
